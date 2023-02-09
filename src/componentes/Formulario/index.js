dd7383685f55de4d38753075eaf2c96c9dd4ecda
Gabriel <gabrielnovatoo@gmail.com>
2023-02-09 19:12:49 -0300

    Create button w/ submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,58 @@
+import { useState } from 'react'
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = [
+        'TOP',
+        'JUNGLE',
+        'MID',
+        'ADC',
+        'SUP'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [elo, setElo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [posicao, setPosicao] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido =>', nome, elo, imagem)
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do jogador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu nome"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..."/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome" 
+                    valor={nome} 
+                    aoAlterado={valor => setNome(valor)}/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Elo" 
+                    placeholder="Digite sua Elo" 
+                    valor={elo} 
+                    aoAlterado={valor => setElo(valor)}/>
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem..." 
+                    valor={imagem} 
+                    aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Posição" 
+                    itens={times}
+                    valor={posicao}
+                    aoAlterado={valor => setPosicao(valor)}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
